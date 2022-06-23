ddcfe6f0f1518784eead761f3bef187f25cb168b
Produccion <produccion@froxa.com>
2022-06-23 19:37:20 +0200

    staratr

--- window_app/index.py
+++ window_app/index.py
@@ -1,16 +1,21 @@
-from tkinter import *
+import tkinter as tk
 
-def clicked(textLabel):
-        textLabel.configure(text = "I just got clicked")
 
 def start_window_app(root):
-    # place a label on the root window
-    textLabel = root.Label(root, text="Hello, World!") 
-    textLabel.grid() 
+    root.rowconfigure(4, minsize=50)
+    root.columnconfigure([0, 1, 2, 3], minsize=50)
+   
     
-    btn = Button(root, text = "Click me" ,
-             fg = "red", command=clicked)
+
+    textLabel0 = tk.Label(text="") 
+    textLabel0.grid(row=0, column=0) 
+    textLabel = tk.Label( text="Hello, World!") 
+    textLabel.grid(row=1, column=1) 
+    textLabel1 = tk.Label( text="Hello, World!") 
+    textLabel1.grid(row=2, column=2) 
+    
     # textLabel.pack()
-    root.title('Server App')
+    root.title('Gráficos Froxa')
     root.geometry('350x200')
+   
     root.mainloop()
